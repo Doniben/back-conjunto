37024594473414ca4ca364d3f895b7776c018dd7
JARG85 <jahir85rodriguez@hotmail.com>
2021-12-05 22:27:23 -0500

    soft change without functions

--- backend/src/config.js
+++ backend/src/config.js
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 config();
 
 export default {
-  MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://smilebang:smilebang@cluster0.eauza.mongodb.net/books-api?retryWrites=true&w=majority",
+  MONGODB_URI: "mongodb+srv://smilebang:smilebang@cluster0.eauza.mongodb.net/books-api?retryWrites=true&w=majority",
   PORT: process.env.PORT || 4000,
   SECRET: 'books-api'
 };
