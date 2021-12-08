63e03ca78597d1f363146551f9bd6d8361049eef
JARG85 <jahir85rodriguez@hotmail.com>
2021-12-08 11:44:50 -0500

    added function search with matchs words

--- backend/src/controllers/books.controller.js
+++ backend/src/controllers/books.controller.js
@@ -44,14 +44,62 @@ export const getBookById = async (req, res) => {
 
 export const getBookBytitle = async (req, res) => {
   const { title } = req.body;
-  console.log(req.body);
-  const book = await Book.find({ title: title });
-  res.status(200).json(book);
+  try {
+    const allBooks = await Book.find().then((response)=>{
+      let resultado = [];
+      let memoria = [];
+      const titulo = response.filter(res => res.title = title);
+      if (titulo) resultado.push(titulo);
+      let minuscula = title.toLowerCase();
+      const fragmentacion = minuscula.split(" ");
+      
+      const filtro = () =>{
+        fragmentacion.map(x => {
+          if (x.length > 3) memoria.push(x);
+        })
+        //filter repeat words
+        memoria = Array.from(new Set(memoria));
+
+        //run array search matchs 
+        memoria.map((x) => {
+          let filtrador = new RegExp(x,"ig");
+          const busqueda = response.filter(element => filtrador.test(element.title));
+          console.log(busqueda)
+          if (busqueda.length > 0 ) resultado.push(busqueda);
+        });
+
+        resultado = resultado.reduce((ccl, ob) => (ccl.concat(ob)), []);
+        resultado = Array.from(new Set(resultado));
+        
+        if(resultado.length > 0 ) res.status(200).json({resultados:resultado})
+        else res.status(200).json({resultados:"no hay resultados"})
+        
+      }
+      filtro();
+    })  
+    .catch((err) => {console.log(err)});
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({erro:"in the request"})
+  }
+
+
 };
 
 export const getBooks = async (req, res) => {
-  const books = await Book.find();
-  return res.json(books);
+ 
+    const books = await Book.find()
+    .then((response)=>{
+      console.log("fine")
+      res.json(response);
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500);
+    })
+
+  
 };
 
 export const updateBookById = async (req, res) => {
