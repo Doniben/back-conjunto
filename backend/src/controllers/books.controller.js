c73ac20a3dbf73faba4374511b8e0937e8130f04
JARG85 <jahir85rodriguez@hotmail.com>
2021-12-16 12:30:52 -0500

    fomatter and improve functionality

--- backend/src/controllers/books.controller.js
+++ backend/src/controllers/books.controller.js
@@ -43,124 +43,124 @@ export const getBookById = async (req, res) => {
 };
 
 export const getBookBytitle = async (req, res) => {
-  const { title, isbn, subtitle, autor, category, publicationDate, editor } = req.body;
+  const { title, isbn, subtitle, autor, category, publicationDate, editor } =
+    req.body;
   try {
-    const allBooks = await Book.find().then((response)=>{
-      let resultado = [];
+    const allBooks = await Book.find()
+      .then((response) => {
+        let resultado = [];
 
-      if(isbn){
-        const serial = response.filter(res  => res.isbn == isbn);
-        if (serial > 0) resultado.push(serial);
-      }else{
+        if (isbn) {
+          const serial = response.filter((res) => res.isbn == isbn);
+          if (serial > 0) resultado.push(serial);
+        } else {
+          let controlador = [];
 
-        let controlador = [];
+          if (title) controlador.push("title");
+          if (subtitle) controlador.push("subtitle");
+          if (autor) controlador.push("autor");
+          if (category) controlador.push("category");
+          if (publicationDate) controlador.push("publicationDate");
+          if (editor) controlador.push("editor");
 
-        if(title) controlador.push("title");
-        if(subtitle) controlador.push("subtitle");
-        if(autor) controlador.push("autor");
-        if(category) controlador.push("category");
-        if(publicationDate)controlador.push("publicationDate");
-        if(editor) controlador.push("editor");
+          let contador = 0;
 
-        let contador = 0;
+          //cicle that creator of a filter secuential
+          controlador.forEach((atributo) => {
+            let memoria = [];
+            let minuscula = "";
 
-        //cicle that creator of a filter secuential 
-        controlador.forEach(atributo => {
-          let memoria = [];
-          let minuscula = ""
+            contador += 1;
 
-          contador += 1;
-          
-          switch (atributo) {
-            case "title":
-              minuscula = title;
-              break;
-            case "subtitle":
-              minuscula = subtitle;
-              break;
-            case "autor":
-              minuscula = autor;
-              break;
-            case "category":
-              minuscula = category;
-              break;
-            case "publicationDate":
-            minuscula = publicationDate;
-              break;
-            case "editor":
-              minuscula = editor;
+            switch (atributo) {
+              case "title":
+                minuscula = title;
                 break;
-          }   
-
-          minuscula = minuscula.toLowerCase();
-          const fragmentacion = minuscula.split(" ");
-          fragmentacion.map(x => {
-            if (x.length > 3) memoria.push(x);
-          })
-          //filter repeat words
-          memoria = Array.from(new Set(memoria));
-    
-          const sub_proceso = (x, element, atributo) => {
-            let filtrador = new RegExp(x,"ig");
-            let analisis = filtrador.test(element[atributo]);
-            if (analisis) return analisis
-          }
-    
-          //run array search matchs based count
-          memoria.map((x) => {
-            let busqueda = null;
-            if(resultado.length == 0 & contador == 1){
-               busqueda = response.filter(element => sub_proceso(x, element, atributo));
-               resultado.push(busqueda);
-            }else{                     
-               busqueda = resultado.filter(element => sub_proceso(x, element, atributo));
-               resultado=[];
-               resultado.push(busqueda);
+              case "subtitle":
+                minuscula = subtitle;
+                break;
+              case "autor":
+                minuscula = autor;
+                break;
+              case "category":
+                minuscula = category;
+                break;
+              case "publicationDate":
+                minuscula = publicationDate;
+                break;
+              case "editor":
+                minuscula = editor;
+                break;
             }
-            
+
+            minuscula = minuscula.toLowerCase();
+            const fragmentacion = minuscula.split(" ");
+            fragmentacion.map((x) => {
+              if (x.length > 3) memoria.push(x);
+            });
+            //filter repeat words
+            memoria = Array.from(new Set(memoria));
+
+            const sub_proceso = (x, element, atributo) => {
+              let filtrador = new RegExp(x, "ig");
+              let analisis = filtrador.test(element[atributo]);
+              if (analisis) return analisis;
+            };
+
+            //run array search matchs based count
+            memoria.map((x) => {
+              let busqueda = null;
+              if ((resultado.length == 0) & (contador == 1)) {
+                busqueda = response.filter((element) =>
+                  sub_proceso(x, element, atributo)
+                );
+                resultado.push(busqueda);
+              } else {
+                busqueda = resultado.filter((element) =>
+                  sub_proceso(x, element, atributo)
+                );
+                resultado = [];
+                resultado.push(busqueda);
+              }
+            });
+
+            //unidimencional array and clear repeat elements
+            resultado = resultado.reduce((ccl, ob) => ccl.concat(ob), []);
+            resultado = Array.from(new Set(resultado));
+
+            const titulo = response.filter((res) => res.title == title);
+            if (titulo > 0) resultado.push(titulo);
+
+            //response based results
+            if (atributo == controlador[controlador.length - 1]) {
+              if (resultado.length > 0) {
+                res.status(200).json(resultado);
+              } else {
+                res.status(200).json({ respusta: "no hay resultados" });
+              }
+            }
           });
-
-          //unidimencional array and clear repeat elements
-          resultado = resultado.reduce((ccl, ob) => (ccl.concat(ob)), []);
-          resultado = Array.from(new Set(resultado));
-          
-          const titulo = response.filter(res => res.title == title);
-          if (titulo>0) resultado.push(titulo);
-
-          //response based results
-          if(resultado.length > 0){
-            res.status(200).json(resultado);
-          }else{
-            res.status(200).json({respusta:"no hay resultados"});
-          }
-
-        })
-     
-      }
-
-    })  
-    .catch((err) => {console.log(err)});
-    
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   } catch (error) {
     console.log(error);
-    res.status(500).json({erro:"in the request"})
+    res.status(500).json({ erro: "in the request" });
   }
-
 };
 
 export const getBooks = async (req, res) => {
- 
-    const books = await Book.find()
-    .then((response)=>{
-      console.log("fine")
+  const books = await Book.find()
+    .then((response) => {
+      console.log("fine");
       res.json(response);
     })
-    .catch(err => {
-      console.log(err)
+    .catch((err) => {
+      console.log(err);
       res.status(500);
-    })
-
-  
+    });
 };
 
 export const updateBookById = async (req, res) => {
