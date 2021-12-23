5f5555f6c0db28a3b68dafff5b533733d9602530
JARG85 <jahir85rodriguez@hotmail.com>
2021-12-23 09:16:54 -0500

    created funtion of filter and cards to alert

--- front/src/views/BusquedaLibros.js
+++ front/src/views/BusquedaLibros.js
@@ -16,6 +16,7 @@ import {
   ModalHeader,
   ModalBody,
   ModalFooter,
+  Alert,
 } from "reactstrap";
 
 import { ToastContainer, toast } from "react-toastify";
@@ -60,7 +61,9 @@ function BusquedaLibros() {
 
   const [field_isbn, setField_isbn] = useState(false);
   const handleIsbn = () => {
-    titulo();
+    setAutor("");
+    setCategory("");
+    setPublishDate("");
     setField_isbn(!field_isbn);
   };
 
@@ -77,6 +80,11 @@ function BusquedaLibros() {
     titulo();
   };
 
+  const [Card_no_results_internal, setCard_no_results_internal] =
+    useState(false);
+  const [Card_no_results_external, setCard_no_results_external] =
+    useState(false);
+
   //funtion search and set values internals and external
   const busquedaGeneral = async () => {
     //prevent open filters when search
@@ -85,17 +93,56 @@ function BusquedaLibros() {
     setIsCategoria(false);
     setIsFecha(false);
 
-    //Request internal DB
-    const url_Int = "https://api-books-tp.herokuapp.com/api/books/titulo"; //URI server
-    let body = {};
-    if (mainText != "") {
-      if (field_isbn) {
-        body = { isbn: mainText };
-      } else {
-        if (Autor != "") body.autor = Autor;
-        if (Category != "") body.category = Category;
-        if (PublishDate != "") body.publicationDate = PublishDate;
+    let modificacion; // handle response of results for make a filter
+    const filtrado = (body, response) => {
+      modificacion = response;
+      //component that make a filter
+      const filtro_general = (respuesta, parametro, atributo) => {
+        const match = (element) => {
+          let expresion = new RegExp(parametro, "ig");
+          let analisis = expresion.test(element);
+          if (analisis) return analisis;
+        };
+        const resultado = respuesta.filter((elemento) =>
+          match(elemento[atributo])
+        );
+        return resultado;
+      };
+
+      if (modificacion.length > 0) {
+        if (body.category != "") {
+          modificacion = filtro_general(modificacion, body.category, "subject");
+        }
+        if (body.publicationDate != "") {
+          modificacion = filtro_general(
+            modificacion,
+            body.publicationDate,
+            "publish_year"
+          );
+        }
+        if (modificacion.length > 0) {
+          setCard_no_results_external(false);
+          setResultadosExternos(modificacion);
+        }
       }
+
+      if (modificacion == 0) {
+        setCard_no_results_external(true);
+        setResultadosExternos(modificacion);
+      }
+    };
+
+    //Request internal DB
+    const url_Int = "http://localhost:4000/api/books/titulo"; //URI server
+
+    //Request external DB
+    const url_Ext = "https://openlibrary.org/";
+
+    let body = {};
+
+    //funtion define for search and filter
+    const funcion_de_busqueda = async (URI, body) => {
+      //Request internal
       try {
         const dataInterna = await axios({
           method: "POST",
@@ -103,33 +150,57 @@ function BusquedaLibros() {
           data: JSON.stringify(body), // <---- This step it is important
           url: url_Int,
         }).then((response) => {
-          setResultadosInternos(response.data);
-          console.log(response.data);
+          if (response.data.length > 0) {
+            setCard_no_results_internal(false);
+            setResultadosInternos(response.data);
+          } else {
+            setCard_no_results_internal(true);
+          }
         });
       } catch (error) {
         console.log(error);
       }
 
-      //Request external DB
-      const url_Ext = "https://openlibrary.org/search.json?q=";
-      const uri_Ext = url_Ext + mainText;
-
-      console.log(mainText);
-
+      //Request External with call a filter
       try {
-        const data = await axios.get(uri_Ext).then((response) => {
-          response = response.data;
-          console.log(response);
-          setResultadosExternos(response.docs);
+        const data = await axios.get(URI).then((response) => {
+          response = response.data.docs;
+          if ((body.category != "") | (body.publicationDate != "")) {
+            filtrado(body, response);
+          }
         });
       } catch (error) {
         console.log(error);
       }
+    };
+
+    //make request second params of search
+    let uri_ext = "";
+    if (mainText != "") {
+      if (field_isbn) {
+        uri_ext = `${url_Ext}isbn/${mainText}`;
+        body.isbn = mainText;
+        funcion_de_busqueda(uri_ext, body);
+      } else {
+        uri_ext = `${url_Ext}search.json?title=${mainText}`;
+        body.title = mainText;
+        if (Autor != "") body.autor = Autor;
+        if (Category != "") body.category = Category;
+        if (PublishDate != "") body.publicationDate = PublishDate;
+        funcion_de_busqueda(uri_ext, body);
+      }
+    } else if (Autor != "") {
+      uri_ext = `${url_Ext}search.json?author=${Autor}`;
+      body.autor = Autor;
+      if (Category != "") body.category = Category;
+      if (PublishDate != "") body.publicationDate = PublishDate;
+      funcion_de_busqueda(uri_ext, body);
     }
   };
 
   //hooks for control modal component
   const [modal, setModal] = useState(false);
+  const [modalInternal, setModalInternal] = useState(false);
   const [infoModal, setInfomodal] = useState({});
   const [descripcion, setDescripcion] = useState("");
 
@@ -167,6 +238,14 @@ function BusquedaLibros() {
     setModal(!modal);
   };
 
+  const handleModalInternal = async (prop) => {
+    setInfomodal(prop);
+    setModalInternal(!modalInternal);
+  };
+
+  const tokenAccess =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxYTliZjU2ZDZhYjhlOGEzYTI3NTNlMyIsImlhdCI6MTY0MDI2MzIwNiwiZXhwIjoxNjQwMzQ5NjA2fQ.GLUilPihwoNg6VrVKA5x77TPnvfGPqjuk501dRTb3nU";
+
   //function that get values of modal and placed in db internal
   const passOutInt = async () => {
     const urlInt = "http://localhost:4000/api/books/";
@@ -191,8 +270,7 @@ function BusquedaLibros() {
         method: "POST",
         headers: {
           "content-type": "application/json",
-          "x-access-token":
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxYjliY2Y4NDc3ZTQxMTg0Y2QyYTRmYyIsImlhdCI6MTYzOTU3Njc3NywiZXhwIjoxNjM5NjYzMTc3fQ.2xdvkQGlZFRSP06bAp_NwB_FOoHEw9VKaADD-osKAoY",
+          "x-access-token": tokenAccess,
         },
         data: JSON.stringify(body), // <---- This step it is important
         url: urlInt,
@@ -204,6 +282,28 @@ function BusquedaLibros() {
       });
     } catch (error) {
       console.log(error);
+    }
+  };
+
+  //function delete book
+  const deletebook = async () => {
+    const url = "http://localhost:4000/api/books/" + infoModal["_id"];
+    try {
+      const peticionEliminacion = await axios({
+        method: "DELETE",
+        headers: {
+          "x-access-token": tokenAccess,
+        }, // <---- This step it is important
+        url: url,
+      }).then((response) => {
+        const notify = () => toast("El libro fue eliminado correctamente");
+        notify();
+        setModal(false);
+        busquedaInterna();
+      });
+    } catch (error) {
+      console.log(error);
+      alert("someThing was happened");
     }
   };
 
@@ -235,12 +335,20 @@ function BusquedaLibros() {
                 </InputGroup>
                 <div>
                   <Button
+                    color="info"
+                    onClick={busquedaGeneral}
+                    style={{ marginBottom: "1rem" }}
+                  >
+                    Buscar
+                  </Button>
+                  <Button
                     color="primary"
                     onClick={toggle}
                     style={{ marginBottom: "1rem" }}
                   >
                     Filtros
                   </Button>
+
                   <Collapse isOpen={isOpen}>
                     <Card>
                       <CardBody>
@@ -250,61 +358,97 @@ function BusquedaLibros() {
                           <div>
                             <Button onClick={handleIsbn}>ISBN</Button>
                             <Button onClick={openAutor}>Autor</Button>
-                            <Button onClick={openCategoria}>Categoria</Button>
-                            <Button onClick={openFecha}>
-                              Fecha de publicion
-                            </Button>
+                            {(mainText != "") | (Autor != "") ? (
+                              <Button onClick={openCategoria}>Categoria</Button>
+                            ) : (
+                              ""
+                            )}
+                            {(mainText != "") | (Autor != "") ? (
+                              <Button onClick={openFecha}>
+                                Fecha de publicion
+                              </Button>
+                            ) : (
+                              ""
+                            )}
                           </div>
                         )}
                       </CardBody>
                     </Card>
                   </Collapse>
                 </div>
+                {field_isbn ? (
+                  ""
+                ) : (
+                  <div>
+                    <Collapse isOpen={isOpenAutor}>
+                      <Card>
+                        <CardBody>
+                          <InputGroup>
+                            <Input
+                              value={field_isbn ? "" : Autor}
+                              onChange={handleAutorInput}
+                            />
+                          </InputGroup>
+                          <p>Escriba el autor</p>
+                        </CardBody>
+                      </Card>
+                    </Collapse>
 
-                <Collapse isOpen={isOpenAutor}>
-                  <Card>
-                    <CardBody>
-                      <InputGroup>
-                        <Input value={Autor} onChange={handleAutorInput} />
-                      </InputGroup>
-                      <p>Escriba el autor</p>
-                    </CardBody>
-                  </Card>
-                </Collapse>
+                    {(mainText != "") | (Autor != "") ? (
+                      <div>
+                        <Collapse isOpen={isCategoria}>
+                          <Card>
+                            <CardBody>
+                              <InputGroup>
+                                <Input
+                                  value={field_isbn ? "" : Category}
+                                  onChange={handleCategoryInput}
+                                />
+                              </InputGroup>
+                              <p>Escriba categoria</p>
+                            </CardBody>
+                          </Card>
+                        </Collapse>
 
-                <Collapse isOpen={isCategoria}>
-                  <Card>
-                    <CardBody>
-                      <InputGroup>
-                        <Input
-                          value={Category}
-                          onChange={handleCategoryInput}
-                        />
-                      </InputGroup>
-                      <p>Escriba categoria</p>
-                    </CardBody>
-                  </Card>
-                </Collapse>
-
-                <Collapse isOpen={isFecha}>
-                  <Card>
-                    <CardBody>
-                      <InputGroup>
-                        <Input
-                          value={PublishDate}
-                          onChange={handlePublishDateInput}
-                        />
-                      </InputGroup>
-                      <p>Escriba la fecha</p>
-                    </CardBody>
-                  </Card>
-                </Collapse>
+                        <Collapse isOpen={isFecha}>
+                          <Card>
+                            <CardBody>
+                              <InputGroup>
+                                <Input
+                                  value={field_isbn ? "" : PublishDate}
+                                  onChange={handlePublishDateInput}
+                                />
+                              </InputGroup>
+                              <p>Escriba la fecha</p>
+                            </CardBody>
+                          </Card>
+                        </Collapse>
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                )}
 
                 <h5 className="title">Encuentra tu libro</h5>
                 <p className="category">
                   Recuerda que es una base de datos libre{" "}
                   <a href="https://nucleoapp.com/?ref=1712">Library</a>
                 </p>
+                {Card_no_results_internal ? (
+                  <Alert color="info">
+                    No hay resultados de busqueda en base interna
+                  </Alert>
+                ) : (
+                  ""
+                )}
+                {Card_no_results_external ? (
+                  <Alert color="info">
+                    No hay resultados de busqueda en bases externas
+                  </Alert>
+                ) : (
+                  ""
+                )}
               </CardHeader>
               <CardBody className="all-icons">
                 <Row>
@@ -338,7 +482,14 @@ function BusquedaLibros() {
                               ? prop.publicationDate + " "
                               : "No hay fecha"}
                           </p>
-                          <Button>Visualizar</Button>
+                          <Button
+                            color="success"
+                            onClick={() => {
+                              handleModalInternal(prop);
+                            }}
+                          >
+                            Visualizar
+                          </Button>
                         </div>
                       </Col>
                     );
@@ -373,9 +524,7 @@ function BusquedaLibros() {
                           <p style={{ color: "black" }}>titulo: {prop.title}</p>
                           <p style={{ color: "black" }}>
                             nombre/s autore/s:{" "}
-                            {prop.author_name
-                              ? prop.author_name + " "
-                              : "anonimo"}
+                            {prop.author_name ? prop.author_name[0] : "anonimo"}
                           </p>
                           <p style={{ color: "black" }}>
                             fecha de publicacion:{" "}
@@ -402,6 +551,7 @@ function BusquedaLibros() {
           </Col>
         </Row>
 
+        {/* modalExternal */}
         <div>
           <Modal isOpen={modal} toggle={handleModal}>
             <ModalHeader toggle={handleModal}>{infoModal.title}</ModalHeader>
@@ -447,6 +597,62 @@ function BusquedaLibros() {
                 Agregar a base de datos
               </Button>{" "}
               <Button color="secondary" onClick={handleModal}>
+                Cerrar
+              </Button>
+            </ModalFooter>
+          </Modal>
+        </div>
+
+        {/* modalInternal */}
+        <div>
+          <Modal isOpen={modalInternal} toggle={handleModalInternal}>
+            <ModalHeader toggle={handleModalInternal}>
+              {infoModal.title}
+            </ModalHeader>
+            <ModalBody>
+              {infoModal.image ? (
+                <img src={infoModal.image} />
+              ) : (
+                <img src="https://image.freepik.com/foto-gratis/libro-lupa_127657-11249.jpg" />
+              )}
+              <br />
+              <h4>Descripcion</h4>
+              {infoModal.description
+                ? infoModal.description
+                : "no hay descripcion"}
+              <br />
+              <br />
+              <p>
+                Autor :{" "}
+                {infoModal.autor ? infoModal.autor + " " : "No hay autor"}
+              </p>
+              <p>
+                Fecha de publicacion:{" "}
+                {infoModal.publicationDate
+                  ? infoModal.publicationDate[0]
+                  : "No hay fecha de publicacion"}
+              </p>
+              <p>
+                Editorial:{" "}
+                {infoModal.editor
+                  ? infoModal.editor[0]
+                  : "No hay registro de editorial"}
+              </p>
+              <p>
+                categorias:{" "}
+                {infoModal.category
+                  ? infoModal.category + " "
+                  : "No hay categorias"}
+              </p>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="danger" onClick={deletebook}>
+                Eliminar de base de datos
+              </Button>{" "}
+              <Button color="info" onClick={handleModalInternal}>
+                Editar
+              </Button>
+              <Button color="secondary" onClick={handleModalInternal}>
                 Cerrar
               </Button>
             </ModalFooter>
