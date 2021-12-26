19c40cad04c9994feef5fb4eb7c8a5bb1708b465
JARG85 <jahir85rodriguez@hotmail.com>
2021-12-25 19:56:02 -0500

    created landing page

---
+++ front/src/views/HomePage.js
@@ -0,0 +1,173 @@
+import React, { useState } from "react";
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption,
+  Card,
+  Button,
+  CardImg,
+  CardTitle,
+  CardText,
+  CardGroup,
+  CardSubtitle,
+  CardBody,
+} from "reactstrap";
+
+const items = [
+  {
+    id: 1,
+    altText: "Slide 1",
+    caption: "Slide 1",
+  },
+  {
+    id: 2,
+    altText: "Slide 2",
+    caption: "Slide 2",
+  },
+  {
+    id: 3,
+    altText: "Slide 3",
+    caption: "Slide 3",
+  },
+];
+
+const Example = (props) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
+
+  const slides = items.map((item) => {
+    return (
+      <CarouselItem
+        className="custom-tag"
+        tag="div"
+        key={item.id}
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+      >
+        <CarouselCaption
+          className="text-danger"
+          captionText={item.caption}
+          captionHeader={item.caption}
+        />
+      </CarouselItem>
+    );
+  });
+
+  return (
+    <div>
+      <div>
+        <style>
+          {`.custom-tag {
+              max-width: 100%;
+              height: 500px;
+              background: black;
+            }`}
+        </style>
+        <Carousel activeIndex={activeIndex} next={next} previous={previous}>
+          <CarouselIndicators
+            items={items}
+            activeIndex={activeIndex}
+            onClickHandler={goToIndex}
+          />
+          {slides}
+          <CarouselControl
+            direction="prev"
+            directionText="Previous"
+            onClickHandler={previous}
+          />
+          <CarouselControl
+            direction="next"
+            directionText="Next"
+            onClickHandler={next}
+          />
+        </Carousel>
+      </div>
+
+      <CardGroup>
+        <Card>
+          <CardImg
+            top
+            width="100%"
+            height="300px"
+            src="https://i.imgur.com/7XACIrm.jpg"
+            alt="Card image cap"
+          />
+          <CardBody>
+            <CardTitle tag="h5">Biblioteca colaborativa</CardTitle>
+            <CardSubtitle tag="h6" className="mb-2 text-muted">
+              Card subtitle
+            </CardSubtitle>
+            <CardText>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardImg
+            top
+            width="100%"
+            height="300px"
+            src="https://i.imgur.com/bB8p8Jv.jpg"
+            alt="Card image cap"
+          />
+          <CardBody>
+            <CardTitle tag="h5">Card title</CardTitle>
+            <CardSubtitle tag="h6" className="mb-2 text-muted">
+              Card subtitle
+            </CardSubtitle>
+            <CardText>
+              This card has supporting text below as a natural lead-in to
+              additional content.
+            </CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardImg
+            top
+            width="100%"
+            height="300px"
+            src="https://i.imgur.com/DbnjUIA.jpg"
+            alt="Card image cap"
+          />
+          <CardBody>
+            <CardTitle tag="h5">Card title</CardTitle>
+            <CardSubtitle tag="h6" className="mb-2 text-muted">
+              Card subtitle
+            </CardSubtitle>
+            <CardText>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This card has even longer content
+              than the first to show that equal height action.
+            </CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
+      </CardGroup>
+    </div>
+  );
+};
+
+export default Example;
